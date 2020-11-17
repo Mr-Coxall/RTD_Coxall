d77a2e55683148c4b1742e7a0b090be96d03e1b9
Ubuntu <ubuntu@ip-172-31-45-237.ec2.internal>
2020-11-17 04:02:16 +0000

    init

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,45 @@ html_logo = "images/cs-logo.png"
 #htmlhelp_basename = 'MusicforGeeksandNerdsdoc'
 #html_show_sourcelink = False
 
+# -- Options for LaTeX output --------------------------------------------------
 
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+#'papersize': 'letterpaper',
+
+# The font size ('10pt', '11pt' or '12pt').
+#'pointsize': '10pt',
+
+# Additional stuff for the LaTeX preamble.
+#'preamble': '',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_documents = [
+  ('index', 'toga.tex', u'Computer Based Problem Solving',
+   u'Patrick Coxall', 'manual'),
+]
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+#latex_logo = None
+
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
+
+# If true, show page references after internal links.
+#latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+#latex_show_urls = False
+
+# Documents to append as an appendix to all manuals.
+#latex_appendices = []
+
+# If false, no module index is generated.
+#latex_domain_indices = True
 
 
 ################################################################################
